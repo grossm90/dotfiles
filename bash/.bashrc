a70edd609b2e582550ad23db4e6f8860e80e94fd
Matthew L. Gross <dev@matthewlgross.com>
2026-09-20 18:12:52 -0400

    Add mdoc and update treesitter

--- bash/.bashrc
+++ bash/.bashrc
@@ -11,3 +11,5 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 source /usr/share/nvm/init-nvm.sh
+
+export PATH="$HOME/.local/bin:$PATH"
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,37 @@
 return {
 	{
-        "nvim-treesitter/nvim-treesitter", 
-        branch = 'master', 
-        lazy = false, 
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                auto_install = true,
-                indent = {
-                    enable = true
-                },
-                highlight = {
-                    enable = true
-                }
-            })
-        end
-    }
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local ts = require("nvim-treesitter")
+			ts.setup({})
+
+			local function enable(buf, lang)
+				vim.treesitter.start(buf, lang)
+				vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local buf = args.buf
+					local lang = vim.treesitter.language.get_lang(args.match)
+					if not lang then
+						return
+					end
+					-- Parser already installed: turn on highlight + indent
+					if pcall(enable, buf, lang) then
+						return
+					end
+					-- Otherwise try to install it, then enable
+					ts.install({ lang }):await(function()
+						if vim.api.nvim_buf_is_valid(buf) then
+							pcall(enable, buf, lang)
+						end
+					end)
+				end,
+			})
+		end,
+	},
 }
